a32b9947d08824ee805af9f699205e7473d5a48a
ks12b0000 <ks12b0000@gmail.com>
2022-10-14 20:40:01 +0900

    2022-10-14

---
+++ book-backend/src/main/java/com/cos/bookbackend/web/BookController.java
@@ -0,0 +1,40 @@
+package com.cos.bookbackend.web;
+
+import com.cos.bookbackend.domain.Book;
+import com.cos.bookbackend.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class BookController {
+
+    private final BookService bookService;
+
+    @PostMapping("/book")
+    public ResponseEntity<?> save(@RequestBody Book book) {
+        return new ResponseEntity<>(bookService.저장하기(book), HttpStatus.CREATED); // 201을 응답.
+    }
+
+    @GetMapping("/book")
+    public ResponseEntity<?> findAll() {
+        return new ResponseEntity<>(bookService.모두가져오기(), HttpStatus.OK); // 200을 응답.
+    }
+
+    @GetMapping("/book/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        return new ResponseEntity<>(bookService.한건가져오기(id), HttpStatus.OK); // 200을 응답.
+    }
+
+    @PutMapping("/book/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Book book) {
+        return new ResponseEntity<>(bookService.수정하기(id, book), HttpStatus.OK); // 200을 응답.
+    }
+
+    @DeleteMapping("/book/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id, @RequestBody Book book) {
+        return new ResponseEntity<>(bookService.삭제하기(id), HttpStatus.OK); // 200을 응답.
+    }
+}
